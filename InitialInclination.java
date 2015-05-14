5b33995fbcb129f60c0adc754534febf4c37987c
2797jake <jake.oakes27@gmail.com>
2015-05-14 09:08:32 -0500

    First upload

---
+++ InitialInclination.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jake on 5/14/2015.
+ */
+public class InitialInclination
+{
+	public InitialInclination(double research, int food, int population, int wood, int stone, )
+	{
+
+	}
+}
---
+++ Runner.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Jake on 5/14/2015.
+ */
+public class Runner
+{
+	public static void main(String args[])
+	{
+
+
+
+
+	}
+}
